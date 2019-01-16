49cb8ec4b5ee6607bf90d0f775d49352351122c6
Rohan <rohanbhokare92@gmail.com>
2019-01-16 21:08:01 +0530

    UI improved detais view added

---
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -0,0 +1,29 @@
+import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from './../../_services/alertify.service';
+import { UserService } from './../../_services/user.service';
+import { Component, OnInit } from '@angular/core';
+import { User } from './../../_model/user';
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
+
+@Component({
+  selector: 'app-member-list',
+  templateUrl: './member-list.component.html',
+  styleUrls: ['./member-list.component.css']
+})
+export class MemberListComponent implements OnInit {
+  users: User[];
+  constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.users = data['users'];
+    });
+  }
+  // loadUsers() {
+  //   this.userService.getUsers().subscribe((users: User[]) => {
+  //     this.users = users;
+  //   }, error => {
+  //     this.alertify.error(error);
+  //   });
+  // }
+}
